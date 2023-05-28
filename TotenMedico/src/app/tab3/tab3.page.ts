2efa3d9dd705663b344802fe3814f0ab7b4f4321
Nascimento <jacksoncarlos2015@gmail.com>
2023-05-27 23:38:45 -0300

    Atualização page 3

--- TotenMedico/src/app/tab3/tab3.page.ts
+++ TotenMedico/src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,40 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController, private http: HttpClient) {}
+
+  async fazerChamadaAPI() {
+    try {
+      const response = await this.http.get<any>('URL_DA_API').toPromise();
+      const mensagem = response.message;
+      return mensagem;
+      // Faça algo com a mensagem obtida da API
+    } catch (error) {
+      console.error(error);
+      // Lida com o erro, se necessário
+    }
+  }
+
+  async exibirAlerta() {
+    const mensagem = await this.fazerChamadaAPI();
+
+    const alert = await this.alertController.create({
+      header: 'Senha Chamada:',
+      message: mensagem, // TODO: Alterar depois para consumir do back
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async exibirAlertaErro() {
+    const alert = await this.alertController.create({
+      header: 'Erro',
+      message: 'Erro no APP',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
 }
