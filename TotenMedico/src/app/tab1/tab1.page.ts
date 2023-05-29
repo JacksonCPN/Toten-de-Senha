a067720c7e679d885300caf8040ba467712c220b
Nascimento <jacksoncarlos2015@gmail.com>
2023-05-28 21:33:20 -0300

    Atualização

--- TotenMedico/src/app/tab1/tab1.page.ts
+++ TotenMedico/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
+  cadastrarSenha(tipoSenha: string) {
+    const url = 'http://localhost:3000/api/senha';
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
 
+    const data = { tipoSenha: tipoSenha };
+    
+
+
+    this.http.post(url, data, { headers: headers })
+      .subscribe(
+        response => {
+          console.log('Requisição bem-sucedida', response, data);
+        },
+        error => {
+          console.error('Erro na requisição', error);
+        }
+      );
+  }
 }
